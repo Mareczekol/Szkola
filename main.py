50b7fbc489bc794ca46cbe080f5413695e37d619
Mareczekol <mareczek2024@gmail.com>
2023-07-11 19:29:41 +0200

    Utworzono plik main.py, dodano do repozytorium

---
+++ main.py
@@ -0,0 +1,27 @@
+# program do obslugi bazy szkolnej
+
+
+while True:
+    print("Dostepne opcje:")
+    print("utworz")
+    print("zarzadzaj")
+    print("koniec")
+
+    command = input("Wybierz opcje: ")
+
+    if command == "utworz":
+        print("Dostepne opcje:")
+        print("uczen")
+        print("nauczyciel")
+        print("koniec")
+
+
+
+
+
+
+
+# komenda koniec
+    if command == "Koniec":
+        print("Zamykanie")
+        break
